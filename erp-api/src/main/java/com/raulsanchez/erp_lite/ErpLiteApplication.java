5465c1516a0a18e4f2f40f3796f3b42b3334cfe2
Raul Sanchez <raulsr.dev@gmail.com>
2026-05-19 22:03:07 +0200

    01-modules: estructura multi-módulo Gradle

--- erp-api/src/main/java/com/raulsanchez/erp_lite/ErpLiteApplication.java
+++ erp-api/src/main/java/com/raulsanchez/erp_lite/ErpLiteApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ErpLiteApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ErpLiteApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ErpLiteApplication.class, args);
+    }
 }
